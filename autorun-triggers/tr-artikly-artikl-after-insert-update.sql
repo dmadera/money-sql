f7f9b5ddd3fd8bfad7375b4523179d628179f914
Daniel Maděra <madera.dan@gmail.com>
2021-05-26 11:40:02 +0200

    funkcni - pomaly artikly

--- autorun-triggers/tr-artikly-artikl-after-insert-update.sql
+++ autorun-triggers/tr-artikly-artikl-after-insert-update.sql
@@ -4,12 +4,47 @@ AFTER INSERT, UPDATE
 AS
 BEGIN
 
+	DECLARE @VychoziCenik_ID AS UNIQUEIDENTIFIER;
+	SET @VychoziCenik_ID = (SELECT TOP 1 Agenda.VychoziCenik_ID FROM System_AgendaDetail AS Agenda);
+
+	DECLARE @VychoziSklad_ID AS UNIQUEIDENTIFIER;
+	SET @VychoziSklad_ID = (SELECT TOP 1 Agenda.VychoziSklad_ID FROM System_AgendaDetail AS Agenda);
+
+	DISABLE TRIGGER ALL ON Ceniky_PolozkaCeniku;
+	
+	UPDATE Ceniky_PolozkaCeniku SET 
+		SkladovaCena_UserData = StavCena.JednotkovaSkladovaCena,
+		Marze_UserData = IIF(StavCena.JednotkovaSkladovaCena = 0, 0, ROUND(100/StavCena.JednotkovaSkladovaCena*(StavCena.JednotkovaCenikovaCena-StavCena.JednotkovaSkladovaCena), 2)),
+		Cena = ROUND(Cena.Cena, 2),
+		CisloDokladu_UserData = ISNULL(Pohyb.CisloDokladu, ''),
+		Sklad_ID = @VychoziSklad_ID,
+		NepodlehatSleveDokladu = IIF(Cena.Cenik_ID = @VychoziCenik_ID, 0, 1),
+		DatumZmenyZasoby_UserData = IIF(Pohyb.Datum IS NULL, '', FORMAT(Pohyb.Datum, 'yyyy.MM.dd HH:mm:ss'))
+	FROM Ceniky_PolozkaCeniku AS Cena
+	INNER JOIN inserted AS Artikl ON Artikl.ID = Cena.Artikl_ID
+	INNER JOIN Sklady_Zasoba AS Zasoba ON Zasoba.Artikl_ID = Artikl.ID
+	INNER JOIN CSW_BI_StavSkladuVCenach AS StavCena ON StavCena.Artikl_ID = Cena.Artikl_ID AND StavCena.Sklad_ID = Cena.Sklad_ID
+	LEFT JOIN (
+		SELECT 
+			Pohyb.Konto_ID,
+			ISNULL(MAX(Pohyb.Modify_Date), MAX(Pohyb.Create_Date)) AS Datum
+		FROM S5_Sklady_PohybZasobyPrehled AS Pohyb
+		WHERE Pohyb.DruhPohybu = 0
+		GROUP BY Pohyb.Konto_ID
+	) AS SubPohyb ON SubPohyb.Konto_ID = Zasoba.ID
+	LEFT JOIN (
+		SELECT Konto_ID, CisloDokladu, ISNULL(Modify_Date, Create_Date) AS Datum
+		FROM S5_Sklady_PohybZasobyPrehled
+	) AS Pohyb ON Pohyb.Konto_ID = SubPohyb.Konto_ID AND Pohyb.Datum = SubPohyb.Datum;
+
+	ENABLE TRIGGER ALL ON Ceniky_PolozkaCeniku;
+
 	UPDATE Artikly_Artikl SET	
-		NakupniCena_UserData = StavCena.JednotkovaSkladovaCena,
-		Marze_UserData = IIF(StavCena.JednotkovaSkladovaCena = 0, 0, ROUND(100/StavCena.JednotkovaSkladovaCena*(StavCena.JednotkovaCenikovaCena-StavCena.JednotkovaSkladovaCena), 2))
+		NakupniCena_UserData = Cena.SkladovaCena_UserData,
+		Marze_UserData = Cena.Marze_UserData
 	FROM Artikly_Artikl AS Artikl
 	INNER JOIN inserted ON inserted.ID = Artikl.ID
-	INNER JOIN CSW_BI_StavSkladuVCenach AS StavCena ON StavCena.Artikl_ID = Artikl.ID
+	INNER JOIN Ceniky_PolozkaCeniku AS Cena ON Cena.Artikl_ID = Artikl.ID AND Cena.Cenik_ID = @VychoziCenik_ID;
 
 	-- produktove klice - spoji jako retezec do UserData pole
 	UPDATE Artikly_Artikl SET 
@@ -83,13 +118,14 @@ BEGIN
 	INNER JOIN Artikly_ArtiklJednotka AS ArtJed ON ArtJed.ID = Artikl.NakupniJednotka_ID;
 
 	-- jednotky - nastavi u vedlejsich jednotek aktualizovanych artiklu nedelitelne mnozstvi na 0
+	-- nastavi sazbu DPH
 	UPDATE Artikly_ArtiklJednotka SET
 		Artikly_ArtiklJednotka.NedelitelneMnozstvi = 0.0
 	FROM Artikly_ArtiklJednotka AS Jednotka
 	INNER JOIN inserted ON inserted.ID = Jednotka.Parent_ID
 	WHERE ParentJednotka_ID IS NOT NULL;
 
-	-- nastavi sazbu DPH
+	
 	UPDATE Artikly_Artikl SET
 		SazbaDPH_UserData = CONCAT(STR(Sazba.Sazba, 2, 0), '%')
 	FROM Artikly_Artikl AS Art
@@ -141,63 +177,14 @@ BEGIN
 	) AS SQ ON SQ.ID = Art.ID
 	OPTION (MAXRECURSION 3);
 	
-	-- zapise k zasobe denni prodej AVG a MED
-
-	DECLARE @PocetDniMinZasoby AS INT;
-	SET @PocetDniMinZasoby = 20;
-
 	UPDATE Sklady_Zasoba SET
 		BaleniMnozstvi_UserData = Artikl.BaleniMnozstvi_UserData,
 		BaleniJednotky_UserData = Artikl.BaleniJednotky_UserData,
 		Priznaky_UserData = Artikl.Priznaky_UserData,
 		NakupniCena_UserData = Artikl.NakupniCena_UserData,
-		Marze_UserData = Artikl.Marze_UserData,
-		ProdejMinAVG_UserData = CASE
-			WHEN PocitatProdej = 0 THEN -1
-			WHEN ZasobaAVG.Zasoba_ID IS NULL THEN 0
-			ELSE ROUND(ZasobaAVG.SumMnozstvi / DATEDIFF(dd, ZasobaAVG.PrvniPohyb, GETDATE()) * @PocetDniMinZasoby, 0) END,
-		ProdejMinMED_UserData = CASE
-			WHEN PocitatProdej = 0 THEN -1
-			WHEN ZasobaMED.Zasoba_ID IS NULL THEN 0
-			ELSE ROUND(ZasobaMED.SumMedian / DATEDIFF(dd, ZasobaMED.PrvniPohyb, GETDATE()) * @PocetDniMinZasoby, 0) END
+		Marze_UserData = Artikl.Marze_UserData
 	FROM Sklady_Zasoba AS Zasoba
 	INNER JOIN inserted ON inserted.ID = Zasoba.Artikl_ID
-	INNER JOIN Artikly_Artikl AS Artikl ON Artikl.ID = Zasoba.Artikl_ID
-	LEFT JOIN (
-		SELECT 
-			Artikl.ID AS ID,
-			ISNULL(MIN(CAST(PocitatProdejZasob_UserData AS INT)), 0) AS PocitatProdej,
-			MIN(Artikl.NakupniCena_UserData) AS NakupniCena_UserData,
-			MIN(Marze_UserData) AS Marze_UserData
-		FROM Artikly_Artikl AS Artikl
-		LEFT JOIN Artikly_ArtiklProduktovyKlic AS ArtProdKlic ON ArtProdKlic.Parent_ID = Artikl.ID
-		LEFT JOIN Artikly_ProduktovyKlic AS ProdKlic ON ProdKlic.ID = ArtProdKlic.ProduktovyKlic_ID
-		GROUP BY Artikl.ID
-	) AS Artikl1 ON Artikl1.ID = Zasoba.Artikl_ID
-	LEFT JOIN (
-		SELECT 
-			Pohyb.Konto_ID AS Zasoba_ID, 
-			SUM(Pohyb.Mnozstvi) AS SumMnozstvi,
-			MIN(Pohyb.Datum) AS PrvniPohyb
-		FROM Sklady_PohybZasoby AS Pohyb
-		WHERE Pohyb.DruhPohybu = 1
-		GROUP BY Pohyb.Konto_ID
-	) AS ZasobaAVG ON ZasobaAVG.Zasoba_ID = Zasoba.ID
-	LEFT JOIN (
-		SELECT 
-			Zas.ID AS Zasoba_ID, 
-			MAX(Median.Median) * COUNT(Zas.ID) AS SumMedian, 
-			Min(Median.DatumPohybu) AS PrvniPohyb
-		FROM Sklady_Zasoba AS Zas
-		INNER JOIN (
-			SELECT 
-				Pohyb.Konto_ID AS Zasoba_ID, 
-				Datum AS DatumPohybu, 
-				PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY Mnozstvi) OVER (PARTITION BY Pohyb.Konto_ID) AS Median 
-			FROM Sklady_PohybZasoby AS Pohyb
-			WHERE Pohyb.DruhPohybu = 1
-		) AS Median ON Median.Zasoba_ID = Zas.ID
-		GROUP BY Zas.ID
-	) AS ZasobaMED ON ZasobaMED.Zasoba_ID = Zasoba.ID;
-
+	INNER JOIN Artikly_Artikl AS Artikl ON Artikl.ID = Zasoba.Artikl_ID;
+	
 END
--- autorun-triggers/tr-ceniky-polozka-ceniku-after-insert-update.sql
+++ autorun-triggers/tr-ceniky-polozka-ceniku-after-insert-update.sql
@@ -4,42 +4,10 @@ AFTER INSERT, UPDATE
 AS
 BEGIN
 
-	DECLARE @VychoziCenik_ID AS UNIQUEIDENTIFIER;
-	SET @VychoziCenik_ID = (SELECT TOP 1 Agenda.VychoziCenik_ID FROM System_AgendaDetail AS Agenda);
-
-	DECLARE @VychoziSklad_ID AS UNIQUEIDENTIFIER;
-	SET @VychoziSklad_ID = (SELECT TOP 1 Agenda.VychoziSklad_ID FROM System_AgendaDetail AS Agenda);
-
-	UPDATE Ceniky_PolozkaCeniku SET
-		SkladovaCena_UserData = StavCena.JednotkovaSkladovaCena,
-		Marze_UserData = IIF(StavCena.JednotkovaSkladovaCena = 0, 0, ROUND(100/StavCena.JednotkovaSkladovaCena*(Cena.Cena-StavCena.JednotkovaSkladovaCena), 2)),
-		Cena = ROUND(Cena.Cena, 2),
-		NepodlehatSleveDokladu = IIF(Cena.Cenik_ID = @VychoziCenik_ID, 0, 1),
-		Sklad_ID = @VychoziSklad_ID
-		--CisloDokladu_UserData = ISNULL(Pohyb.CisloDokladu, ''),
-		--DatumZmenyZasoby_UserData = IIF(pohyb.Datum IS NULL, '', FORMAT(Pohyb.Datum, 'yyyy.MM.dd HH:mm:ss'))
-	FROM Ceniky_PolozkaCeniku AS Cena 
-	INNER JOIN inserted ON inserted.ID = Cena.ID
-	INNER JOIN CSW_BI_StavSkladuVCenach AS StavCena ON StavCena.Artikl_ID = Cena.Artikl_ID AND StavCena.Sklad_ID = Cena.Sklad_ID
-	LEFT JOIN Sklady_Zasoba AS Zasoba ON Zasoba.Artikl_ID = Cena.Artikl_ID
-	LEFT JOIN (
-		SELECT 
-			Pohyb.Konto_ID,
-			ISNULL(MAX(Pohyb.Modify_Date), MAX(Pohyb.Create_Date)) AS Datum
-		FROM S5_Sklady_PohybZasobyPrehled AS Pohyb
-		WHERE Pohyb.DruhPohybu = 0
-		GROUP BY Pohyb.Konto_ID
-	) AS SubPohyb ON SubPohyb.Konto_ID = Zasoba.ID
-	LEFT JOIN (
-		SELECT Konto_ID, CisloDokladu, ISNULL(Modify_Date, Create_Date) AS Datum
-		FROM S5_Sklady_PohybZasobyPrehled
-	) AS Pohyb ON Pohyb.Konto_ID = SubPohyb.Konto_ID AND Pohyb.Datum = SubPohyb.Datum
-
-	-- spusti trigger artikly, ktery aktualizuje i skladove zasoby
+	-- spusti trigger artikly, ktery aktualizuje i skladove zasoby a cenikove polozky
 	UPDATE Artikly_Artikl SET
 		Deleted = Artikl.Deleted
 	FROM Artikly_Artikl AS Artikl
-	INNER JOIN inserted AS Cena ON Cena.Artikl_ID = Artikl.ID 
-	WHERE Cena.Cenik_ID = @VychoziCenik_ID;
+	INNER JOIN inserted AS Cena ON Cena.Artikl_ID = Artikl.ID;	
 
 END;
